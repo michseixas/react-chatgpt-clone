e96ecb33e7ecc0a06628c4e033ab3252c91f14d9
Mich Seixas <mcseixas@gmail.com>
2023-06-27 19:14:38 +0200

    first steps, button, primary colors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 const App = () => {
   return (
     <div className="App">
+    <section className="side-bar">
+      <button>+ New Chat</button>
+      <ul className="history"></ul>
+      <nav>
+        <p>Made by Mich</p>
+      </nav>
+    </section>
+    <section className="main"></section>
     </div>
   );
 }
