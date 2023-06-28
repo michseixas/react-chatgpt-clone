dff10387ab01951bb8fd95bde5ec71ce56a9b162
Mich Seixas <mcseixas@gmail.com>
2023-06-28 19:49:16 +0200

    styling of sidebar, button, nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
     <section className="side-bar">
       <button>+ New Chat</button>
-      <ul className="history"></ul>
+      <ul className="history">
+        <li>Ejemplo</li>
+      </ul>
       <nav>
         <p>Made by Mich</p>
       </nav>
