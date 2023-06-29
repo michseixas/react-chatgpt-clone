953ac8f7f1b3ec844d2c8c9bb89e6f772c938e62
Mich Seixas <mcseixas@gmail.com>
2023-06-29 17:57:17 +0200

    firts steps implementation API

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
-//Here we sets up an Express.js server, 
+//Here we sets up an Express.js server,
 //configure it to parse JSON data, enable CORS, and listen on port 8000.
 
-const PORT =  8000
-const express = require('express')
-const cors = require('cors')
-const app = express()
-app.use(express.json()) //allows us to work with json when we start sending sending stuff from the frontend to the backend without post requests
-app.use(cors()) //allows your server to accept requests from different origins or domains. This is useful when your frontend and backend are hosted on different domains, as it ensures that the browser allows the frontend to make requests to the backend.
+const PORT = 8000;
+const express = require("express");
+const cors = require("cors");
+const app = express(); //you can't pass Json from the front to the back unless you have this
+app.use(express.json()); //allows us to work with json when we start sending sending stuff from the frontend to the backend without post requests
+app.use(cors()); //allows your server to accept requests from different origins or domains. This is useful when your frontend and backend are hosted on different domains, as it ensures that the browser allows the frontend to make requests to the backend.
 
-app.listen(PORT, () => console.log('Your server is running on PORT ' + PORT))
+app.post("/completions", async (req, res) => {
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: "How are you?" }],
+      max_tokens: 100,
+    }),
+  };
+  try {
+    //post request to this url
+    const response = await fetch("https://api.openai.com/v1/chat/completions", options);
+    const data = await response.json()
+    res.send(data)
+} catch (error) {
+    console.error(error);
+  }
+});
+
+const API_KEY = "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";
+app.listen(PORT, () => console.log("Your server is running on PORT " + PORT));
